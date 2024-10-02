f66ddb9a9fa3e5b2c95c532dba0a09f441efca12
Aayush Goel <160343928+AayushCS@users.noreply.github.com>
2024-10-01 21:51:10 -0300

    added background hover effect

--- app.js
+++ app.js
@@ -1,6 +1,70 @@
 const reset = document.querySelector('#reset');
 const selectScore = document.querySelector('#selectScore');
 
+window.addEventListener('DOMContentLoaded',()=>{
+    const blockContainer = document.getElementById('blocks')
+    const blockSize = 50;
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const numCols = Math.ceil(screenWidth/blockSize);
+    const numRows = Math.ceil(screenHeight/blockSize);
+    const numBlocks = numCols * numRows;
+    function createBlocks(){
+        for(let i = 0; i < numBlocks; i ++ ){
+            const block = document.createElement('div');
+            block.classList.add('block');
+            block.dataset.index = i;
+            block.addEventListener('mousemove',highlightneighour);
+            blockContainer.append(block);
+        }
+    }
+    
+    function highlightneighour(){
+        const index = parseInt(this.dataset.index);
+        const neighbours = [
+            index - 1 , 
+            index + 1 , 
+            index - numCols,
+            index + numCols,
+            index - numCols - 1,
+            index - numCols + 1,
+            index + numCols - 1,
+            index + numCols + 1
+        ].filter(
+            i => i >= 0 && i < numBlocks && Math.abs((i % numCols) - (index% numCols)) <= 1
+        );
+    
+        this.classList.add('highlight');
+        setTimeout(()=>{
+            this.classList.remove('highlight');
+        }, 500);
+    
+        shuffleArray(neighbours)
+        .slice(0,1)
+        .forEach((nIndex) => {
+            const neighbour = blockContainer.children(nIndex);
+            if(neighbour){ 
+                neighbour.classList.add('highlight');
+                setTimeout(() => {
+                    neighbour.classList.remove('highlight')
+                },500);
+            }
+        });
+    }
+    
+    function shuffleArray(ar){
+        for(let i = arr.length - 1; i > 0; i++){
+            const j = Math.floor(Math.random() * (i+1));
+            [arr[i],arr[j]] = [arr[j], arr[i]]
+        }
+        return arr;
+    } 
+    createBlocks();
+})
+
+
+
+
 const p1 = {
     score : 0,
     button: document.querySelector('#p1button'),
